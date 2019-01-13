c0954d420b9f6d327c93df15aead8cf840d5111b
doohyunkim0304 <ewb.doohyun@gmail.com>
2019-01-14 08:23:45 +0900

    extending schema

---
+++ graphql/resolvers.js
@@ -0,0 +1,15 @@
+const danny = {
+    name: "Danny",
+    age: 31,
+    gender: "male"
+};
+
+const resolvers = {
+    Query: {
+        person: () => danny
+    }
+};
+
+
+
+// export default resovlers;
