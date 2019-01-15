77f7ae0538d11cd3eba1232419863d326a2327ed
doohyunkim0304 <ewb.doohyun@gmail.com>
2019-01-15 22:52:44 +0900

    graphql end

--- graphql/db.js
+++ graphql/db.js
@@ -1,53 +1,19 @@
 // import { strict } from "assert";
+import fetch from "node-fetch";
 
-let movies = [
-    {   
-        id: 0,
-        name: "Starwars",
-        score: 3
-    },
-    {   
-        id: 1,
-        name: "Avengers",
-        score: 6
-    },
-    {   
-        id: 2,
-        name: "Godfather",
-        score: 8
-    },
-    {   
-        id: 3,
-        name: "Logan",
-        score: 8
-    },
+const API_URL = "https://yts.am/api/v2/list_movies.json?";
 
-];
 
-export const getMovies = () => movies;
-
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = API_URL;
+    if(limit > 0){
+        REQUEST_URL += `limit=${limit}`;
+    }
+    if(rating > 0){
+        REQUEST_URL += `&minimum_rating=${rating}`;
+    }
+    return fetch(REQUEST_URL)
+    .then(res => res.json())
+    .then(json => json.data.movies);
 }
 
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if(movies.length > cleanedMovies.length){
-        movies = cleanedMovies;
-        return true;
-    }else {
-        return false;
-    }
-}
-
-
-export const addMovie = (name,score) =>{
-    const newMovie = {
-        id: `${movies.length + 1}`,
-        name,
-        score
-    }
-    movies.push(newMovie);
-    return newMovie;
-};
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,16 +2,10 @@ import { getMovies, getById, addMovie,deleteMovie} from "./db";
 
 const resolvers = {
     Query: {
-        movies: () => getMovies(),
-        movie: (_, {id}) => getById(id)
+        movies: (_, {rating,limit}) => getMovies(limit, rating),
     },
-    Mutation: {
-        addMovie: (_, {name,score}) => addMovie(name, score),
-        deleteMovie: (_,{id}) => deleteMovie(id)
-    }
 };
 
  export default resolvers;
 
 
- 
